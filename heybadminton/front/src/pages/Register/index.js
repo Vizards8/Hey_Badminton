a2f4a1db479ee7f745f144392581d8f40c4a5f0d
NengZhou <Vizards8@outlook.com>
2023-04-05 22:11:32 -0400

    add Register

--- heybadminton/front/src/pages/Register/index.js
+++ heybadminton/front/src/pages/Register/index.js
@@ -1,5 +1,80 @@
-const Register = () => {
-  return <div>Register</div>
-}
+import { Form, Input, Button } from 'antd';
+import './Register.css';
 
-export default Register
+const Register = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className="register-container">
+      <Form
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not a valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input size="large" />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password size="large" />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+              },
+            }),
+          ]}
+        >
+          <Input.Password size="large" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" size="large">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
