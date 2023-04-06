b450752a35cb5904e235683c17590927c61564e4
NengZhou <Vizards8@outlook.com>
2023-04-05 22:11:22 -0400

    add Login

--- heybadminton/front/src/pages/Login/index.js
+++ heybadminton/front/src/pages/Login/index.js
@@ -1,5 +1,64 @@
-const Login = () => {
-  return <div>login</div>
-}
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './LoginForm.css';
 
-export default Login
+const Login = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className="login-form-container">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        size="large" // Set size to large
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="/" style={{ color: '#ffffff' }}>register now!</a>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
