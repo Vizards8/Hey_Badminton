6750da91c1b7a5156f20aa1a309a13d1ec843c94
Rwear <47514441+Rwear@users.noreply.github.com>
2023-04-11 19:32:40 -0400

    equipment

--- heybadminton/front/src/pages/Equipment/index.js
+++ heybadminton/front/src/pages/Equipment/index.js
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import EquipmentItem from './EquipmentItem';
+
+import myImage from "./logo.png";
+
 const Equip = () => {
-  return <div>Equip</div>
+  const equipment = [
+    {
+      name: 'Badminton Racket',
+      description: 'A lightweight racket used to hit the shuttlecock across the net.',
+      image: myImage,
+      price: 50.00
+    },
+    {
+      name: 'Shuttlecock',
+      description: 'A feather or plastic projectile hit back and forth over the net by players.',
+      image: myImage,
+      price: 10.00
+    },
+    {
+      name: 'Badminton Shoes',
+      description: 'Specialized shoes with good grip to help players move quickly and change direction.',
+      image: myImage,
+      price: 75.00
+    }
+  ];
+
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleSelectItem = (item) => {
+    setSelectedItem(item);
+  }
+
+  return (
+    <div>
+      <h2>Badminton Equipment</h2>
+      <p>Here is a list of the equipment available for use:</p>
+      <div className="equipment-list">
+        {equipment.map(item => (
+          <EquipmentItem key={item.name} item={item} onSelect={handleSelectItem} />
+        ))}
+      </div>
+      {selectedItem && (
+        <p>You have selected: {selectedItem.name}</p>
+      )}
+    </div>
+  )
 }
 
-export default Equip
+export default Equip;
