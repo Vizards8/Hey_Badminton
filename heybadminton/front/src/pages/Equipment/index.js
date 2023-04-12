c9c194f02e09029f68336c82464be775d4f86995
NengZhou <Vizards8@outlook.com>
2023-04-12 17:33:19 -0400

    modify Equipment

--- heybadminton/front/src/pages/Equipment/index.js
+++ heybadminton/front/src/pages/Equipment/index.js
@@ -1,11 +1,9 @@
-import { Layout, Breadcrumb, List, Avatar} from "antd";
+import { Layout, Breadcrumb, List, Avatar } from "antd";
 import { HomeOutlined } from "@ant-design/icons";
 
 import "./Equipment.css";
 
-import maomao from "@/assets/images/profile.png";
-
-const {Content} = Layout;
+const { Content } = Layout;
 
 const data = [
   {
@@ -13,60 +11,65 @@ const data = [
     title: "Topic 1",
     description: "This is the first topic",
     author: "John Doe",
-    image: maomao,
+    image: "https://picsum.photos/50?random=1",
   },
   {
     id: 2,
     title: "Topic 2",
     description: "This is the second topic",
     author: "Jane Smith",
-    image: maomao,
+    image: "https://picsum.photos/50?random=2",
   },
   {
     id: 3,
     title: "Topic 3",
     description: "This is the third topic",
     author: "Bob Johnson",
-    image: maomao,
+    image: "https://picsum.photos/50?random=3",
   },
   {
     id: 1,
     title: "Topic 1",
     description: "This is the first topic",
     author: "John Doe",
-    image: maomao,
+    image: "https://picsum.photos/50?random=1",
   },
   {
     id: 2,
     title: "Topic 2",
     description: "This is the second topic",
     author: "Jane Smith",
-    image: maomao,
+    image: "https://picsum.photos/50?random=2",
   },
   {
     id: 3,
     title: "Topic 3",
     description: "This is the third topic",
     author: "Bob Johnson",
-    image: maomao,
+    image: "https://picsum.photos/50?random=3",
   },
 ];
 
 function Equipment() {
   return (
     <div className="equipment-wrapper">
-      
       <Layout className="layout">
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0', alignItems: 'center' }}>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '2px' }}>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0", alignItems: "center" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "2px",
+              }}
+            >
               <HomeOutlined />
             </div>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>Tournament</Breadcrumb.Item>
           </Breadcrumb>
         </Content>
-      </Layout> 
+      </Layout>
 
       <List
         itemLayout="horizontal"
@@ -81,10 +84,8 @@ function Equipment() {
           </List.Item>
         )}
       />
-
     </div>
   );
 }
-
 
 export default Equipment;
