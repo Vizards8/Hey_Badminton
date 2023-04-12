91c9c6d3ee4673367098022f8099ebafd544d5f8
NengZhou <Vizards8@outlook.com>
2023-04-12 16:11:21 -0400

    modify Equipment

--- heybadminton/front/src/pages/Equipment/index.js
+++ heybadminton/front/src/pages/Equipment/index.js
@@ -1,111 +1,76 @@
-import { Layout, Breadcrumb, List, Avatar } from 'antd';
+import { Layout, Breadcrumb, List, Avatar } from "antd";
+import "./Equipment.css";
 
-import maomao from "@/assets/images/profile.png"
-
-// import logo from "./logo.png"
-
-const {Content} = Layout;
+import maomao from "@/assets/images/profile.png";
 
 const data = [
   {
-    title: 'Topic 1',
-    description: 'This is the first topic',
-    author: 'John Doe',
+    id: 1,
+    title: "Topic 1",
+    description: "This is the first topic",
+    author: "John Doe",
     image: maomao,
   },
   {
-    title: 'Topic 2',
-    description: 'This is the second topic',
-    author: 'Jane Smith',
+    id: 2,
+    title: "Topic 2",
+    description: "This is the second topic",
+    author: "Jane Smith",
     image: maomao,
   },
   {
-    title: 'Topic 3',
-    description: 'This is the third topic',
-    author: 'Bob Johnson',
+    id: 3,
+    title: "Topic 3",
+    description: "This is the third topic",
+    author: "Bob Johnson",
     image: maomao,
   },
   {
-    title: 'Topic 1',
-    description: 'This is the first topic',
-    author: 'John Doe',
+    id: 1,
+    title: "Topic 1",
+    description: "This is the first topic",
+    author: "John Doe",
     image: maomao,
   },
   {
-    title: 'Topic 2',
-    description: 'This is the second topic',
-    author: 'Jane Smith',
+    id: 2,
+    title: "Topic 2",
+    description: "This is the second topic",
+    author: "Jane Smith",
     image: maomao,
   },
   {
-    title: 'Topic 3',
-    description: 'This is the third topic',
-    author: 'Bob Johnson',
-    image: maomao,
-  },
-  {
-    title: 'Topic 1',
-    description: 'This is the first topic',
-    author: 'John Doe',
-    image: maomao,
-  },
-  {
-    title: 'Topic 2',
-    description: 'This is the second topic',
-    author: 'Jane Smith',
-    image: maomao,
-  },
-  {
-    title: 'Topic 3',
-    description: 'This is the third topic',
-    author: 'Bob Johnson',
-    image: maomao,
-  },
-  {
-    title: 'Topic 1',
-    description: 'This is the first topic',
-    author: 'John Doe',
-    image: maomao,
-  },
-  {
-    title: 'Topic 2',
-    description: 'This is the second topic',
-    author: 'Jane Smith',
-    image: maomao,
-  },
-  {
-    title: 'Topic 3',
-    description: 'This is the third topic',
-    author: 'Bob Johnson',
+    id: 3,
+    title: "Topic 3",
+    description: "This is the third topic",
+    author: "Bob Johnson",
     image: maomao,
   },
 ];
 
 function Equipment() {
   return (
-    <Layout className="layout">
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>Equipment</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">
-          <List
-            itemLayout="horizontal"
-            dataSource={data}
-            renderItem={(item) => (
-              <List.Item>
-                <List.Item.Meta
-                  avatar={<Avatar src={item.image} />}
-                  title={<a href="#">{item.title}</a>}
-                  description={item.description}
-                />
-              </List.Item>
-            )}
-          />
-        </div>
-      </Content>
-    </Layout>
+    <div className="equipment-wrapper">
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Equipment</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src={item.image} />}
+                title={<a href={`/posts/${item.id}`}>{item.title}</a>}
+                description={item.description}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   );
 }
 
