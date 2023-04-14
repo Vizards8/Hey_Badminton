ec0258aff860304c3949cd92d2087e9f0a7f35c2
Rwear <20172333127@m.scnu.edu.cn>
2023-04-14 00:04:23 -0400

    add pagination

--- heybadminton/front/src/pages/Equipment/index.js
+++ heybadminton/front/src/pages/Equipment/index.js
@@ -1,56 +1,466 @@
-import { Layout, Breadcrumb, List, Avatar } from "antd";
+import { Layout, Breadcrumb, List, Avatar, Pagination } from "antd";
+import React, { useState } from 'react';
 import { HomeOutlined } from "@ant-design/icons";
 
 import "./Equipment.css";
 
 const { Content } = Layout;
 
-const data = [
-  {
-    id: 1,
-    title: "Topic 1",
-    description: "This is the first topic",
-    author: "John Doe",
-    image: "https://picsum.photos/50?random=1",
-  },
-  {
-    id: 2,
-    title: "Topic 2",
-    description: "This is the second topic",
-    author: "Jane Smith",
-    image: "https://picsum.photos/50?random=2",
-  },
-  {
-    id: 3,
-    title: "Topic 3",
-    description: "This is the third topic",
-    author: "Bob Johnson",
-    image: "https://picsum.photos/50?random=3",
-  },
-  {
-    id: 1,
-    title: "Topic 1",
-    description: "This is the first topic",
-    author: "John Doe",
-    image: "https://picsum.photos/50?random=1",
-  },
-  {
-    id: 2,
-    title: "Topic 2",
-    description: "This is the second topic",
-    author: "Jane Smith",
-    image: "https://picsum.photos/50?random=2",
-  },
-  {
-    id: 3,
-    title: "Topic 3",
-    description: "This is the third topic",
-    author: "Bob Johnson",
-    image: "https://picsum.photos/50?random=3",
-  },
-];
-
 function Equipment() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [data] = useState([
+    {
+    id: 1,
+    title: "Topic 1",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 2,
+    title: "Topic 2",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 3,
+    title: "Topic 3",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 1,
+    title: "Topic 1",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 2,
+    title: "Topic 2",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 3,
+    title: "Topic 3",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 1,
+    title: "Topic 1",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 2,
+    title: "Topic 2",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 3,
+    title: "Topic 3",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  {
+    id: 4,
+    title: "Topic 4",
+    description: "This is the first topic",
+    author: "John Doe",
+    image: "https://picsum.photos/50?random=1",
+  },
+  {
+    id: 5,
+    title: "Topic 5",
+    description: "This is the second topic",
+    author: "Jane Smith",
+    image: "https://picsum.photos/50?random=2",
+  },
+  {
+    id: 6,
+    title: "Topic 6",
+    description: "This is the third topic",
+    author: "Bob Johnson",
+    image: "https://picsum.photos/50?random=3",
+  },
+  ]);
+  const pageSize = 10;
+
+  const handleChangePage = (page) => {
+    setCurrentPage(page);
+  }
+
+  const startIdx = (currentPage - 1) * pageSize;
+  const endIdx = startIdx + pageSize;
+  const currentData = data.slice(startIdx, endIdx);
+
   return (
     <div className="equipment-wrapper">
       <Layout className="layout">
@@ -70,24 +480,46 @@ function Equipment() {
             </Breadcrumb.Item>
             <Breadcrumb.Item>
               Equipment
-            </Breadcrumb.Item>
+            </Breadcrumb.Item> 
           </Breadcrumb>
         </Content>
       </Layout>
 
       <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={item.image} />}
-              title={<a href={`/post/${item.id}`}>{item.title}</a>}
-              description={item.description}
-            />
-          </List.Item>
-        )}
+  itemLayout="horizontal"
+  dataSource={currentData}
+  renderItem={(item) => (
+    <List.Item>
+      <List.Item.Meta
+        avatar={<Avatar src={item.image} />}
+        title={<a href={`/post/${item.id}`}>{item.title}</a>}
+        description={item.description}
       />
+    </List.Item>
+  )}
+/>
+<Pagination
+  current={currentPage}
+  pageSize={pageSize}
+  total={data.length}
+  onChange={handleChangePage}
+/>
+
+
+      {/* <ul>
+        {currentData.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      <Pagination
+        current={currentPage}
+        pageSize={pageSize}
+        total={data.length}
+        onChange={handleChangePage}
+      /> */}
+
+
+
     </div>
   );
 }
