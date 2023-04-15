f8cb1ec86e80f2109d5e9b60d03ab5aeb60c34b6
WillSilent <will990211@163.com>
2023-04-15 05:27:03 -0400

    lastest version

--- heybadminton/front/src/pages/Home/index.js
+++ heybadminton/front/src/pages/Home/index.js
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 import React from 'react'
-import { Layout, Menu, Breadcrumb, Carousel, Button, Card, Avatar, Divider } from 'antd'
+import { Layout, Carousel, Card, Avatar, Divider, Button } from 'antd'
 import { StarOutlined } from '@ant-design/icons'
 
-const { Content } = Layout
-=======
-import React from 'react';
-import { Layout, Carousel, Card, Avatar, Divider } from 'antd';
-
-import "./Home.css";
+import "./Home.css"
 
 const { Meta } = Card
-const { Content } = Layout;
->>>>>>> f42d7abfecce4a562e4671be5d4ccc3b175e8d6f
+const { Content } = Layout
 
 const HomePage = () => {
+
   const carouselImages = [
     { id: 1, src: 'https://bwfworldchampionships.bwfbadminton.com/wp-content/uploads/sites/8/2022/09/WC2023_Digital_WC-Site_1024x481_3.jpg' },
     { id: 2, src: 'https://bwfworldtour.bwfbadminton.com/wp-content/uploads/sites/11/2019/11/wt_banner_oct2019.jpg' },
@@ -42,7 +36,7 @@ const HomePage = () => {
         </Card>
       ))}
     </div>
-  );
+  )
 
   const EquipmentList = ({ Equipments }) => (
     <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
@@ -58,7 +52,7 @@ const HomePage = () => {
         </Card>
       ))}
     </div>
-  );
+  )
 
 
 
@@ -171,90 +165,40 @@ const HomePage = () => {
   ]
 
   return (
-<<<<<<< HEAD
     <Layout className="layout">
       <Content style={{ padding: '0 50px' }}>
         <div className="site-layout-content" style={{ 'margin-top': '20px' }}>
-          <h1>Welcome to our website!</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque gravida velit, quis tincidunt quam placerat sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-=======
-    <div className="equipment-wrapper">
+          <div className="equipment-wrapper">
 
-      <Content style={{}}>
-        <div className="site-layout-content">
-          {/* <h1>Welcome to our website!</h1> */}
-          <Divider style={{ borderColor: 'black' }}><h2>Smash your way to greatness - Play Badminton!</h2></Divider>
-          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque gravida velit, quis tincidunt quam placerat sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> */}
-          <Carousel autoplay>
-            {carouselImages.map((image) => (
-              <div key={image.id}>
-                <img src={image.src} alt={`Slide ${image.id}`} style={{ width: '960px', height: '420px', margin: 'auto' }} />
-              </div>
-            ))}
-          </Carousel>
+            <Content style={{}}>
+              <div className="site-layout-content">
+                {/* <h1>Welcome to our website!</h1> */}
+                <Divider style={{ borderColor: 'black' }}><h2>Smash your way to greatness - Play Badminton!</h2></Divider>
+                {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque gravida velit, quis tincidunt quam placerat sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p> */}
+                <Carousel autoplay>
+                  {carouselImages.map((image) => (
+                    <div key={image.id}>
+                      <img src={image.src} alt={`Slide ${image.id}`} style={{ width: '960px', height: '420px', margin: 'auto' }} />
+                    </div>
+                  ))}
+                </Carousel>
 
-
-
-          <Divider orientation="left" style={{ borderColor: 'black' }}>
-            <h2>Equipment Products</h2>
-          </Divider>
-
-          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-            <EquipmentList Equipments={Equipments} />
-          </div>
-
-          <Divider orientation="left" style={{ borderColor: 'black' }}><h2>Courtmate</h2></Divider>
-          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-            <CourtmateList courtmates={Courtmates} />
-
-          </div>
-        </div>
-
-      </Content>
-
-    </div>
-  );
-};
->>>>>>> f42d7abfecce4a562e4671be5d4ccc3b175e8d6f
-
-          <Divider orientation="left"><h2>Tournament</h2></Divider>
-          <Carousel autoplay>
-            {carouselImages.map((image) => (
-              <div key={image.id}>
-                <img src={image.src} alt={`Image ${image.id}`} style={{ width: '800px', height: '312.5px', margin: 'auto' }} />
-              </div>
-            ))}
-          </Carousel>
-
-          <Divider orientation="left"><h2>Equipment</h2></Divider>
-          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-            {products.map((product) => (
-              <Card key={product.id} style={{ width: 250, margin: '16px' }}>
-                <img src={product.image} alt={product.name} style={{ height: 200 }} />
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: '8px 0' }}>
-                  <h3>{product.name}</h3>
+                <Divider orientation="left" style={{ borderColor: 'black' }}>
+                  <h2>Equipment</h2>
+                </Divider>
+                <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                  <EquipmentList Equipments={Equipments} />
                 </div>
-                <p>{product.description}</p>
-                <Button type="primary">View Detail</Button>
-              </Card>
-            ))}
-          </div>
 
-          <Divider orientation="left"><h2>Play together</h2></Divider>
-          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-            {testimonials.map((testimonial) => (
-              <div key={testimonial.id} style={{ width: 300, margin: '16px', padding: '16px', border: '1px solid #f0f0f0' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                  <Avatar src={testimonial.avatar} />
-                  <span>{testimonial.name}</span>
+                <Divider orientation="left" style={{ borderColor: 'black' }}><h2>Courtmate</h2></Divider>
+                <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                  <CourtmateList courtmates={Courtmates} />
+
                 </div>
-                <div style={{ display: 'flex', alignItems: 'center', margin: '8px 0' }}>
-                  <StarOutlined style={{ color: '#ffc107', marginRight: '8px' }} />
-                  <span>{testimonial.rating} stars</span>
-                </div>
-                <p>{testimonial.text}</p>
               </div>
-            ))}
+
+            </Content>
+
           </div>
         </div>
       </Content>
