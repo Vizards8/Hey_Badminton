5ff3da00d772f0a06d1580cdcac5f268b1f4668a
NengZhou <Vizards8@outlook.com>
2023-04-14 22:23:50 -0400

    modify Home page issue

--- heybadminton/front/src/pages/Home/index.js
+++ heybadminton/front/src/pages/Home/index.js
@@ -39,7 +39,7 @@ const HomePage = () => {
   const EquipmentList = ({ Equipments }) => (
     <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
       {Equipments.map((Equipment) => (
-        <Card style={{ width: '30%', margin: '0 1rem 1rem' }} key={Equipment.title}>
+        <Card style={{ width: '30%', margin: '0 1rem 1rem' }} key={Equipment.id}>
           <Meta
             avatar={<Avatar src={Equipment.image} />}
             title={Equipment.title}
