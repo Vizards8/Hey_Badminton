86aec77b6843aa950b8d6e8716865a6d3307d3e0
NengZhou <Vizards8@outlook.com>
2023-04-14 20:05:28 -0400

    add Courtmate

--- heybadminton/front/src/pages/Courtmate/index.js
+++ heybadminton/front/src/pages/Courtmate/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  Layout,
   Form,
   Input,
   Select,
@@ -8,46 +7,52 @@ import {
   Card,
   Button,
   Modal,
+  Typography,
 } from "antd";
-import { floatButtonPrefixCls } from "antd/es/float-button/FloatButton";
+import MyBreadcrumb from "@/common/MyBreadcrumb";
+import "./Courtmate.css";
 
-const { Content } = Layout;
 const { Option } = Select;
+const { Title, Paragraph, Text, Link } = Typography;
 
 const data = [
   {
     id: 1,
     title: "Friendly Match 1",
     time: "9:00am",
-    date: "NA",
+    date: "2023-04-05",
     location: "Location 1",
     participants: "6",
     maxParticipants: "8",
+    note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu turpis malesuada, semper elit sed, gravida leo. Sed ultrices magna in felis volutpat, ut finibus tellus egestas. Donec pulvinar magna id libero egestas convallis. Integer in quam ac magna accumsan consectetur.",
     closed: false,
   },
   {
     id: 2,
     title: "Friendly Match 2",
     time: "10:00am",
-    date: "NA",
+    date: "2023-04-12",
     location: "Location 2",
     participants: "4",
     maxParticipants: "6",
+    note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu turpis malesuada, semper elit sed, gravida leo. Sed ultrices magna in felis volutpat, ut finibus tellus egestas. Donec pulvinar magna id libero egestas convallis. Integer in quam ac magna accumsan consectetur.",
     closed: false,
   },
   {
     id: 3,
     title: "Friendly Match 3",
     time: "11:00am",
-    date: "NA",
+    date: "2023-04-23",
     location: "Location 3",
     participants: "2",
     maxParticipants: "4",
+    note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu turpis malesuada, semper elit sed, gravida leo. Sed ultrices magna in felis volutpat, ut finibus tellus egestas. Donec pulvinar magna id libero egestas convallis. Integer in quam ac magna accumsan consectetur.",
     closed: false,
   },
 ];
 
 const defaultFilter = {
+  date: null,
   time: "all",
   location: "all",
   participants: "all",
@@ -56,13 +61,20 @@ const defaultFilter = {
 
 const Courtmate = () => {
   const [visible, setVisible] = useState(false);
+  const [detailVisible, setDetailVisvible] = useState(false);
   const [matches, setMatches] = useState(data);
   const [filteredMatches, setFilteredMatches] = useState(matches);
   const [filter, setFilter] = useState(defaultFilter);
+  const [selectedMatch, setSelectedMatch] = useState(null);
 
   useEffect(() => {
     let currMatches = [...matches];
-    console.log(filter);
+    if (filter.date) {
+      const formatted_date = filter.date.toISOString().slice(0, 10);
+      currMatches = currMatches.filter(
+        (match) => match.date === formatted_date
+      );
+    }
     if (filter.time && filter.time !== "all") {
       currMatches = currMatches.filter((match) => match.time === filter.time);
     }
@@ -89,117 +101,139 @@ const Courtmate = () => {
     setVisible(false);
   };
 
+  const handleShowDetails = (match) => {
+    setSelectedMatch(match);
+    setDetailVisvible(true);
+  };
+
   return (
-    <Layout>
-      <Content style={{ padding: "50px" }}>
-        <div style={{ display: "flex" }}>
-          <div style={{ flex: 1 }}>
-            <Form layout="vertical">
-              <Form.Item label="Time">
-                <Select
-                  value={filter.time}
-                  onChange={(value) => setFilter({ ...filter, time: value })}
-                >
-                  <Option value="all">All</Option>
-                  <Option value="9:00am">9:00am</Option>
-                  <Option value="10:00am">10:00am</Option>
-                  <Option value="11:00am">11:00am</Option>
-                </Select>
-              </Form.Item>
-              <Form.Item label="Location">
-                <Select
-                  value={filter.location}
-                  onChange={(value) =>
-                    setFilter({ ...filter, location: value })
-                  }
-                >
-                  <Option value="all">All</Option>
-                  <Option value="Location 1">Location 1</Option>
-                  <Option value="Location 2">Location 2</Option>
-                  <Option value="Location 3">Location 3</Option>
-                </Select>
-              </Form.Item>
-              <Form.Item label="Number of players">
-                <Select
-                  value={filter.participants}
-                  onChange={(value) =>
-                    setFilter({ ...filter, participants: value })
-                  }
-                >
-                  <Option value="all">All</Option>
-                  <Option value="2">2</Option>
-                  <Option value="4">4</Option>
-                  <Option value="6">6</Option>
-                </Select>
-              </Form.Item>
-              <Form.Item label="Status">
-                <Select
-                  value={filter.closed}
-                  onChange={(value) => setFilter({ ...filter, closed: value })}
-                >
-                  <Option value="all">All</Option>
-                  <Option value={false}>Open</Option>
-                  <Option value={true}>Close</Option>
-                </Select>
-              </Form.Item>
-            </Form>
-          </div>
-          <div style={{ flex: 2, marginLeft: "50px" }}>
-            <div
-              style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-                gap: "50px",
-              }}
-            >
-              {filteredMatches.map((match) => (
-                <Card
-                  key={match.id}
-                  title={match.title}
-                  extra={<Button onClick={() => setVisible(true)}>Join</Button>}
-                >
-                  <p>Location: {match.location}</p>
-                  <p>Date: {match.date}</p>
-                  <p>Time: {match.time}</p>
-                  <p>
-                    Participants: {match.participants}/{match.maxParticipants}
-                  </p>
-                </Card>
-              ))}
-            </div>
-          </div>
-        </div>
-        <Modal
-          title="Join Match"
-          open={visible}
-          onCancel={() => setVisible(false)}
-          onOk={handleJoin}
-        >
+    <div className="courtmate-wrapper">
+      <MyBreadcrumb paths={["Courtmate"]} />
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <Title level={2}>Filters</Title>
           <Form layout="vertical">
-            <Form.Item
-              label="Name"
-              name="name"
-              rules={[{ required: true, message: "Please input your name!" }]}
-            >
-              <Input />
+            <Form.Item label="Date">
+              <DatePicker
+                value={filter.date}
+                onChange={(value) => setFilter({ ...filter, date: value })}
+              />
             </Form.Item>
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  type: "email",
-                  message: "Please input your email!",
-                },
-              ]}
-            >
-              <Input />
+            <Form.Item label="Time">
+              <Select
+                value={filter.time}
+                onChange={(value) => setFilter({ ...filter, time: value })}
+              >
+                <Option value="all">All</Option>
+                <Option value="9:00am">9:00am</Option>
+                <Option value="10:00am">10:00am</Option>
+                <Option value="11:00am">11:00am</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="Location">
+              <Select
+                value={filter.location}
+                onChange={(value) => setFilter({ ...filter, location: value })}
+              >
+                <Option value="all">All</Option>
+                <Option value="Location 1">Location 1</Option>
+                <Option value="Location 2">Location 2</Option>
+                <Option value="Location 3">Location 3</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="Number of players">
+              <Select
+                value={filter.participants}
+                onChange={(value) =>
+                  setFilter({ ...filter, participants: value })
+                }
+              >
+                <Option value="all">All</Option>
+                <Option value="2">2</Option>
+                <Option value="4">4</Option>
+                <Option value="6">6</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="Status">
+              <Select
+                value={filter.closed}
+                onChange={(value) => setFilter({ ...filter, closed: value })}
+              >
+                <Option value="all">All</Option>
+                <Option value={false}>Open</Option>
+                <Option value={true}>Close</Option>
+              </Select>
             </Form.Item>
           </Form>
-        </Modal>
-      </Content>
-    </Layout>
+          <Button
+            style={{ width: "100%" }}
+            onClick={() => setFilter(defaultFilter)}
+          >
+            Reset
+          </Button>
+        </div>
+        <div style={{ flex: 2, marginLeft: "50px" }}>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
+              gap: "50px",
+            }}
+          >
+            {filteredMatches.map((match) => (
+              <Card
+                key={match.id}
+                title={match.title}
+                extra={
+                  <Button type="primary" onClick={() => setVisible(true)}>
+                    Join
+                  </Button>
+                }
+                onClick={handleShowDetails}
+                hoverable
+              >
+                <p>Date: {match.date}</p>
+                <p>Time: {match.time}</p>
+                <p>Location: {match.location}</p>
+                <p>
+                  Participants: {match.participants}/{match.maxParticipants}
+                </p>
+                <p>Note: {match.note}</p>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Modal
+        title="Join Match"
+        open={visible}
+        onCancel={() => setVisible(false)}
+        onOk={handleJoin}
+      >
+        <Form layout="vertical">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                type: "email",
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
   );
 };
 
