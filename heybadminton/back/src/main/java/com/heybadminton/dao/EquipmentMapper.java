cbe5ae5f8f702304d2c6baeb22476c9b4a018c06
Rwear <20172333127@m.scnu.edu.cn>
2023-04-16 16:53:32 -0400

    equipment sql function

---
+++ heybadminton/back/src/main/java/com/heybadminton/dao/EquipmentMapper.java
@@ -0,0 +1,19 @@
+package com.heybadminton.dao;
+
+import com.heybadminton.entity.Equipment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface EquipmentMapper {
+    List<Equipment> getAllEquipment();
+
+    Equipment getEquipmentById(Long id);
+
+    int insertEquipment(Equipment equipment);
+
+    int updateEquipment(Equipment equipment);
+
+    int deleteEquipmentById(Long id);
+}
