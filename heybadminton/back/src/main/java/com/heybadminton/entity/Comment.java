3abe51a527008b16992193ed480b99405b648952
NengZhou <Vizards8@outlook.com>
2023-04-17 18:28:53 -0400

    Fix the problem of null result of getAll() in EquipmentController

--- heybadminton/back/src/main/java/com/heybadminton/entity/Comment.java
+++ heybadminton/back/src/main/java/com/heybadminton/entity/Comment.java
@@ -1,6 +1,11 @@
 package com.heybadminton.entity;
+import lombok.Data;
+import lombok.ToString;
+
 import java.sql.Timestamp;
 
+@Data
+@ToString
 public class Comment {
     private Long id;
     private Long postId;
@@ -8,64 +13,5 @@ public class Comment {
     private String content;
     private Timestamp createTime;
     private boolean isDelete;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
-    public Long getCommentUserId() {
-        return commentUserId;
-    }
-
-    public void setCommentUserId(Long commentUserId) {
-        this.commentUserId = commentUserId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    public boolean isDelete() {
-        return isDelete;
-    }
-
-    public void setDelete(boolean delete) {
-        isDelete = delete;
-    }
-
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "id=" + id +
-                ", postId=" + postId +
-                ", commentUserId=" + commentUserId +
-                ", content='" + content + '\'' +
-                ", createTime=" + createTime +
-                ", isDelete=" + isDelete +
-                '}';
-    }
+    private String postUserProfile;
 }
